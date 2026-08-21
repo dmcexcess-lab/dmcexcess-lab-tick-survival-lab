6cbf864bd1ba5bdd55f35555bf56daeeea4d4f80
dmcexcess-lab <dmcexcess@gmail.com>
2026-08-20 18:08:44 -0700

    Add System 20 area profile catalog

---
+++ game/scripts/generation/areas/AreaProfileCatalog.gd
@@ -0,0 +1,43 @@
+extends RefCounted
+class_name AreaProfileCatalog
+
+const RURAL_CROSSROADS: StringName = &"rural.crossroads"
+
+func has_profile(profile_id: StringName) -> bool:
+    return profile_id == RURAL_CROSSROADS
+
+func profile(profile_id: StringName) -> Dictionary:
+    if profile_id != RURAL_CROSSROADS:
+        return {}
+    return {
+        "id": RURAL_CROSSROADS,
+        "version": 1,
+        "center_exclusion_radius": 30,
+        "edge_margin": 8,
+        "parcel_gap": 3,
+        "primary_parcel_depth": 28,
+        "secondary_parcel_depth": 32,
+        "frontage_min": 28,
+        "frontage_max": 36,
+        "commercial_count": 3,
+        "residential_count": 6,
+        "farmstead_count": 4,
+        "local_road_spurs": 0,
+        "residential_setback": 3,
+        "farmstead_setback": 8,
+        "commercial_setback": 1,
+        "commercial_archetypes": [
+            &"commercial.gas_station.small",
+            &"commercial.diner.rural_small",
+        ],
+        "residential_archetypes": [
+            &"residential.trailer.singlewide",
+            &"residential.house.farm_small",
+            &"residential.house.farm_large",
+            &"residential.house.compact_laundry",
+        ],
+        "farmstead_archetypes": [
+            &"residential.house.farm_small",
+            &"residential.house.farm_large",
+        ],
+    }
