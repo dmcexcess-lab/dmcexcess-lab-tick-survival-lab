295ec554142b42e2da67f99f29eb74405382d91d
dmcexcess-lab <dmcexcess@gmail.com>
2026-09-07 16:24:17 -0700

    combat: route firearm discharge through system 26 sound

---
+++ game/scripts/simulation/sound/FirearmSoundEmitterAdapter.gd
@@ -0,0 +1,19 @@
+extends RefCounted
+class_name FirearmSoundEmitterAdapter
+
+const Profiles = preload("res://scripts/simulation/sound/SoundEmissionProfileCatalog.gd")
+
+var _firearms: FirearmActionService = null
+var _sound: SpatialSoundService = null
+
+func _init(firearms: FirearmActionService = null, sound: SpatialSoundService = null) -> void:
+    _firearms = firearms
+    _sound = sound
+    if _firearms != null: _firearms.firearm_discharged.connect(_on_discharge)
+
+func is_ready() -> bool:
+    return _firearms != null and _firearms.is_ready() and _sound != null and _sound.is_ready()
+
+func _on_discharge(actor_id: String, serial: int, _firearm_id: String, _round_id: String, cell: Vector2i, power: int) -> void:
+    if not is_ready(): return
+    _sound.emit_sound(Profiles.COMBAT_GUNSHOT, cell, actor_id, "combat.gunshot.%s.%d" % [actor_id, serial], power)
